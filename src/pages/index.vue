e18b78d221fab3ad605e6d69ddd60252edb073b6
Sayyiku <sayyikue@gmail.com>
2022-03-11 16:10:38 +0800

    init

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,50 +1,89 @@
-<script setup lang="ts">
-const name = ref('')
+<script setup lang="ts" >
 
-const router = useRouter()
-const go = () => {
-  if (name.value)
-    router.push(`/hi/${encodeURIComponent(name.value)}`)
+interface BlockState {
+  x: number
+  y: number
+  revealed?: boolean
+  mine?: boolean
+  flagged?: boolean
+  adjacentMines?: number
+
 }
+
+const Width = 10
+const Height = 10
+const state = reactive(
+  Array.from({ length: Height }, (_, y) =>
+    Array.from({ length: Width },
+      (_, x): BlockState => ({
+        x, y,
+      }),
+    ),
+  ),
+)
+
+function generateMines() {
+  for (const row of state) {
+    for (const block of row)
+      block.mine = Math.random() < 0.3
+  }
+}
+
+function updateNumers() {
+  for (const row of state) {
+    for (const block of row) {
+      if (block.mine) continue
+      block.adjacentMines = 0
+      for (const [dx, dy] of [
+        [-1, -1], [-1, 0], [-1, 1],
+        [0, -1], [0, 1],
+        [1, -1], [1, 0], [1, 1],
+      ]) {
+        const x = block.x + dx
+        const y = block.y + dy
+        if (x < 0 || x >= Width || y < 0 || y >= Height) continue
+        if (state[y][x].mine) block.adjacentMines++
+      }
+    }
+  }
+}
+
+function onclick(x: number, y: number) {
+  // eslint-disable-next-line no-console,no-template-curly-in-string
+  console.log('Click at ${x}, ${y}')
+}
+
+generateMines()
+updateNumers()
+
 </script>
 
 <template>
   <div>
-    <div i-carbon-campsite text-4xl inline-block />
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-        Vitesse Lite
-      </a>
-    </p>
-    <p>
-      <em text-sm op75>Opinionated Vite Starter Template</em>
-    </p>
-
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      placeholder="What's your name?"
-      type="text"
-      autocomplete="false"
-      p="x-4 y-2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-
-    <div>
-      <button
-        class="m-3 text-sm btn"
-        :disabled="!name"
-        @click="go"
+    Minesweeper
+    <div p5>
+      <div
+        v-for="row,y in state"
+        :key="y"
       >
-        Go
-      </button>
+        <button
+          v-for="item,x in row"
+          :key="x"
+          w-10
+          h-10
+          hover:bg-gray
+          border
+          :class="item.mine ? 'text-red' : 'text-gray'"
+          @click="onclick(x,y)"
+        >
+          <div v-if="item.mine">
+            💣
+          </div>
+          <div v-else>
+            {{ item.adjacentMines }}
+          </div>
+        </button>
+      </div>
     </div>
   </div>
 </template>
